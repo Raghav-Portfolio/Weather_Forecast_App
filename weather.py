f4e9a9cec3cbad68e500e780c81e370dee86351f
Raghav-Git-Windows <rgs.rox@gmail.com>
2024-10-09 17:54:21 +0530

    Final Code is ready, unpacked data and prepared website

--- weather.py
+++ weather.py
@@ -5,7 +5,7 @@ from backend import get_data
 
 st.title("Weather Forecast for the Next Days")
 
-place = st.text_input('Place: ')
+place = st.text_input('Place:')
 days = st.slider('Forecast Days:', min_value=1, max_value=5,
                  help='Select the number of forecast days')
 
@@ -14,7 +14,24 @@ option = st.selectbox('Select data to view',
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-figure = px.line()
-st.px_chart()
+        if option == 'Temperature':  
+            temperatures = [dict['main']['temp'] for dict in filtered_data]#unpacking the temperature value
+            temperatures = [(i/10) for i in temperatures]#normalise the temperature value
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={'x': 'Date','y': 'Temperature(C)'})
+            st.plotly_chart(figure)
+
+        if option == 'Sky':
+            images = {"Clear":"images/clear.png", "Clouds":"images/cloud.png", 
+                    "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]#unpacking the conditions from the data
+            image_path = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_path , width = 110)
+    
+    except KeyError:
+        print('Please enter a valid location')
